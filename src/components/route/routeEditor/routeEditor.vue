0771d9c48ff966cf2996b842e2140b2369b62ee6
Lanzhijiang <lanzhijiang@foxmail.com>
2025-10-13 12:22:56 +0000

    make routeEditor reuse common editor

--- src/components/route/routeEditor/routeEditor.vue
+++ src/components/route/routeEditor/routeEditor.vue
@@ -1,66 +1,62 @@
 <template>
-  <v-card variant="outlined" class="form-section">
-    <v-card-title class="text-h6 d-flex align-center justify-space-between">
-      Route
-      <div class="d-flex" style="gap: 8px">
-        <v-btn-toggle v-model="view" density="compact" mandatory>
-          <v-btn value="form">Form</v-btn>
-          <v-btn value="json">JSON</v-btn>
-        </v-btn-toggle>
-      </div>
-    </v-card-title>
-
-    <v-card-text v-if="view === 'form'">
-      <v-text-field label="Final" v-model="value.final" />
-      <RuleSetsSelector v-model="ruleSets" />
-      <v-divider class="my-4" />
-      <AdvancedSection>
-        <v-checkbox label="Auto Detect Interface" v-model="value.auto_detect_interface" />
-        <v-checkbox label="Override Android VPN" v-model="value.override_android_vpn" />
-        <v-text-field label="Default Interface" v-model="value.default_interface" />
-        <v-text-field label="Default Mark" v-model="value.default_mark" type="number" />
-        <v-text-field label="Default Domain Resolver" v-model="value.default_domain_resolver" />
-        <v-text-field label="Default Network Strategy" v-model="value.default_network_strategy" />
-        <v-combobox
-          label="Default Network Type"
-          multiple
-          chips
-          v-model="value.default_network_type"
-        />
-        <v-combobox
-          label="Default Fallback Network Type"
-          multiple
-          chips
-          v-model="value.default_fallback_network_type"
-        />
-        <v-text-field label="Default Fallback Delay" v-model="value.default_fallback_delay" />
-      </AdvancedSection>
-      <v-divider class="my-4" />
-      <!-- FIXME -->
-      <!-- <ItemSelector
-        title="Rules"
-        :model-value="value.rule_sets"
-        :create-item="createRule"
-        @update:model-value="value.rule_sets = $event"
-      >
-        <template #form="{ item }">
-          <RouteRuleEditor :modelValue="item" />
-        </template>
-        <template #readonly="{ item }">
-          <pre>{{ item }}</pre>
-        </template>
-      </ItemSelector> -->
-    </v-card-text>
-
-    <v-card-text v-else>
-      <JSONEditor v-model="value" />
-    </v-card-text>
-  </v-card>
+  <Editor
+    v-model="editableValue"
+    title="Route"
+    :start-editable="true"
+    @save="onSave"
+    @cancel="onCancel"
+  >
+    <v-text-field label="Final" v-model="editableValue.final" />
+    <RuleSetsSelector v-model="ruleSets" />
+    <v-divider class="my-4" />
+    <AdvancedSection>
+      <v-checkbox label="Auto Detect Interface" v-model="editableValue.auto_detect_interface" />
+      <v-checkbox label="Override Android VPN" v-model="editableValue.override_android_vpn" />
+      <v-text-field label="Default Interface" v-model="editableValue.default_interface" />
+      <v-text-field label="Default Mark" v-model="editableValue.default_mark" type="number" />
+      <v-text-field
+        label="Default Domain Resolver"
+        v-model="editableValue.default_domain_resolver"
+      />
+      <v-text-field
+        label="Default Network Strategy"
+        v-model="editableValue.default_network_strategy"
+      />
+      <v-combobox
+        label="Default Network Type"
+        multiple
+        chips
+        v-model="editableValue.default_network_type"
+      />
+      <v-combobox
+        label="Default Fallback Network Type"
+        multiple
+        chips
+        v-model="editableValue.default_fallback_network_type"
+      />
+      <v-text-field label="Default Fallback Delay" v-model="editableValue.default_fallback_delay" />
+    </AdvancedSection>
+    <v-divider class="my-4" />
+    <!-- FIXME -->
+    <!-- <ItemSelector
+      title="Rules"
+      :model-value="editableValue.rule_sets"
+      :create-item="createRule"
+      @update:model-value="editableValue.rule_sets = $event"
+    >
+      <template #form="{ item }">
+        <RouteRuleEditor :modelValue="item" />
+      </template>
+      <template #readonly="{ item }">
+        <pre>{{ item }}</pre>
+      </template>
+    </ItemSelector> -->
+  </Editor>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import JSONEditor from '@/components/common/JSONEditor.vue';
+import { ref, computed, watch } from 'vue';
+import Editor from '@/components/common/Editor.vue';
 import AdvancedSection from '@/components/common/advancedSection/advancedSection.vue';
 import ItemSelector from '@/components/common/itemSelector/itemSelector.vue';
 import RuleSetsSelector from '../ruleSetsSelector/ruleSetsSelector.vue';
@@ -76,19 +72,31 @@ const emit = defineEmits<{
   (e: 'update:modelValue', value: Route): void;
 }>();
 
-const view = ref<'form' | 'json'>('form');
-
-const value = computed({
-  get: () => props.modelValue,
-  set: (val) => emit('update:modelValue', val),
-});
+const editableValue = ref<Route>(JSON.parse(JSON.stringify(props.modelValue)));
 
 const ruleSets = computed({
-  get: () => value.value.rule_sets || [],
-  set: (val) => (value.value = { ...value.value, rule_sets: val }),
+  get: () => editableValue.value.rule_sets || [],
+  set: (val) => (editableValue.value = { ...editableValue.value, rule_sets: val }),
 });
 
 const createRule = async (item: RouteRule) => item;
+
+const onSave = () => {
+  emit('update:modelValue', editableValue.value);
+};
+
+const onCancel = () => {
+  editableValue.value = JSON.parse(JSON.stringify(props.modelValue));
+};
+
+// Watch for external changes to reset the editable value
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    editableValue.value = JSON.parse(JSON.stringify(newValue));
+  },
+  { deep: true }
+);
 </script>
 
 <style scoped lang="scss" src="./routeEditor.scss"></style>
