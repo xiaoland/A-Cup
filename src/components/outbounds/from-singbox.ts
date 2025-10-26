a99e0a5b094293cbe1716c44b4c2da980bef3eb1
Lanzhijiang <lanzhijiang@foxmail.com>
2025-10-26 16:29:35 +0800

    Fix from-singbox.ts

--- src/components/outbounds/from-singbox.ts
+++ src/components/outbounds/from-singbox.ts
@@ -8,63 +8,50 @@ type Outbound = z.infer<typeof OutboundSchema>;
 type Credential = z.infer<typeof OutboundSchema.shape.credential>;
 
 export function fromSingbox(singboxOutbound: SingboxOutbound): Outbound {
-  const { tag, multiplex, ...retainedFields } = singboxOutbound;
+  const { tag, multiplex, tls, type, server, server_port ...retainedFields } = singboxOutbound;
 
   const other: { [key: string]: any } = {};
   let credential: Credential | null = null;
+  let credentailSchema: any;
 
   const outboundSchemaKeys = Object.keys(OutboundSchema.shape);
 
-  for (const key in retainedFields) {
-    if (!outboundSchemaKeys.includes(key) && key !== 'type' && key !== 'server' && key !== 'server_port') {
-      other[key] = (retainedFields as any)[key];
-    }
-  }
-
-  switch (retainedFields.type) {
+  switch (type) {
     case 'vless':
-      credential = VlessCredentialSchema.parse({
-        uuid: retainedFields.uuid,
-        flow: retainedFields.flow,
-      });
+      credentailSchema = VlessCredentialSchema
       break;
     case 'vmess':
-      credential = VmessCredentialSchema.parse({
-        uuid: retainedFields.uuid,
-        security: retainedFields.security,
-        alter_id: retainedFields.alter_id,
-      });
+      credentailSchema = VmessCredentialSchema;
       break;
     case 'shadowsocks':
-      credential = ShadowsocksCredentialSchema.parse({
-        method: retainedFields.method,
-        password: retainedFields.password,
-      });
+      credentailSchema = ShadowsocksCredentialSchema;
       break;
     case 'hysteria2':
-      credential = Hysteria2CredentialSchema.parse({
-        password: retainedFields.password,
-        obfs: retainedFields.obfs,
-        obfs_password: retainedFields.obfs_password,
-      });
+      credentailSchema = Hysteria2CredentialSchema;
       break;
+  }
+
+  if (credentialSchema) {
+     credentail = credentailSchema.parse(retainedFields);
   }
 
   if (!credential) {
     throw new Error('Could not create credential for outbound type: ' + retainedFields.type);
   }
 
+  for (const key in retainedFields) {
+    if (!outboundSchemaKeys.includes(key) && !Object.keys(credentialSchema.shape).includes(key)) {
+      other[key] = (retainedFields as any)[key];
+    }
+  }
+
   return {
     name: tag,
-    type: retainedFields.type,
-    server: retainedFields.server,
-    server_port: retainedFields.server_port,
+    type,
+    server,
+    server_port,
     mux: multiplex as any,
     other: other as any,
-    credential,
-    readableBy: [],
-    writeableBy: [],
-    region: '',
-    provider: '',
+    credential
   };
 }
