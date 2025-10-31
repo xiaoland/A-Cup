64c07279f241b78b07939aff375ce8a583418cc0
Lan_zhijiang <lanzhijiang@foxmail.com>
2025-10-31 19:05:57 +0800

    fix(ruleSet): export lost `format` field

--- schemas/ruleset.ts
+++ schemas/ruleset.ts
@@ -1,12 +1,12 @@
-import { z } from 'zod';
-import { HeadlessRuleSchema, type SingBoxRuleSet } from './route';
+import { z } from "zod";
+import { HeadlessRuleSchema, type SingBoxRuleSet } from "./route";
 
 export const RuleSetSchema = z.object({
   id: z.int().optional(),
   readableBy: z.array(z.uuid()),
   writeableBy: z.array(z.uuid()),
   name: z.string(),
-  type: z.enum(['remote', 'inline']),
+  type: z.enum(["remote", "inline"]),
   format: z.string().nullable(),
   content: z.string(),
 });
@@ -15,12 +15,16 @@ export type RuleSet = z.infer<typeof RuleSetSchema>;
 
 export function exportRuleSetToSingBox(ruleSet: RuleSet): SingBoxRuleSet {
   if (!ruleSet.id) {
-    throw new Error("Cannot export RuleSet not inserted")
+    throw new Error("Cannot export RuleSet not inserted");
   }
   return {
     tag: ruleSet.id.toString(),
     type: ruleSet.type,
-    url: ruleSet.type === 'remote' ? ruleSet.content : undefined,
-    rules: ruleSet.type === 'inline' ? z.array(HeadlessRuleSchema).parse(JSON.parse(ruleSet.content)) : undefined,
+    format: ruleSet.type === "remote" ? ruleSet.format : undefined,
+    url: ruleSet.type === "remote" ? ruleSet.content : undefined,
+    rules:
+      ruleSet.type === "inline"
+        ? z.array(HeadlessRuleSchema).parse(JSON.parse(ruleSet.content))
+        : undefined,
   } as SingBoxRuleSet;
 }
